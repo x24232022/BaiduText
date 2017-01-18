2b7a3fe9af252589217c60486de036ee79d76640
XieChen <xieChen@gmail.com>
2017-01-18 16:25:03 +0800

    增添权限管理

--- app/src/main/java/com/example/administrator/baidutext/HomeActivity.java
+++ app/src/main/java/com/example/administrator/baidutext/HomeActivity.java
@@ -1,11 +1,17 @@
 package com.example.administrator.baidutext;
 
 
+import android.Manifest;
+import android.content.pm.PackageManager;
 import android.os.Bundle;
 
 
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
@@ -13,10 +19,11 @@ import com.baidu.location.LocationClient;
 import com.example.administrator.baidutext.utils.ActivityUtils;
 
 
+import java.util.ArrayList;
+import java.util.List;
 
 import butterknife.BindView;
 import butterknife.ButterKnife;
-
 
 
 public class HomeActivity extends AppCompatActivity {
@@ -35,12 +42,58 @@ public class HomeActivity extends AppCompatActivity {
         mLocationClient = new LocationClient(getApplicationContext());
         mLocationClient.registerLocationListener(new MyLocationClient());
 
-       mLocationClient.start();
+        List<String> permissionList = new ArrayList<>();
+        if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
+
+        }
+        if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.READ_PHONE_STATE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.READ_PHONE_STATE);
+
+        }
+        if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.WRITE_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED) {
+            permissionList.add(Manifest.permission.WRITE_EXTERNAL_STORAGE);
+
+        }
+        if (!permissionList.isEmpty()) {
+            String[] pewmissions = permissionList.toArray(new String[permissionList.size()]);
+            ActivityCompat.requestPermissions(HomeActivity.this, pewmissions, 1);
+
+        } else {
+
+            requestLocation();
+        }
 
 
     }
 
+    private void requestLocation() {
+        mLocationClient.start();
+    }
 
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        switch (requestCode) {
+            case 1:
+                if(grantResults.length>0){
+                    for (int result: grantResults){
+                        if(result!=PackageManager.PERMISSION_GRANTED){
+                            Toast.makeText(this, "必须统一此权限才能使用", Toast.LENGTH_SHORT).show();
+                            finish();
+                            return;
+                        }
+                    }
+                    requestLocation();
+
+                }else {
+                    Toast.makeText(this, "未知的错误发生了", Toast.LENGTH_SHORT).show();
+                    finish();
+                }
+            break;
+            default:
+                break;
+        }
+    }
 
     private class MyLocationClient implements BDLocationListener {
         @Override
