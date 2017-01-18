e6d41aa2711b306c33a5b3ea77769c5b93b8f484
XieChen <xieChen@gmail.com>
2017-01-18 20:28:15 +0800

    最后一次提交

--- app/src/main/java/com/example/administrator/baidutext/HomeActivity.java
+++ app/src/main/java/com/example/administrator/baidutext/HomeActivity.java
@@ -4,8 +4,6 @@ package com.example.administrator.baidutext;
 import android.Manifest;
 import android.content.pm.PackageManager;
 import android.os.Bundle;
-
-
 import android.support.annotation.NonNull;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.content.ContextCompat;
@@ -16,8 +14,15 @@ import android.widget.Toast;
 import com.baidu.location.BDLocation;
 import com.baidu.location.BDLocationListener;
 import com.baidu.location.LocationClient;
+import com.baidu.location.LocationClientOption;
+import com.baidu.mapapi.SDKInitializer;
+import com.baidu.mapapi.map.BaiduMap;
+import com.baidu.mapapi.map.MapStatusUpdate;
+import com.baidu.mapapi.map.MapStatusUpdateFactory;
+import com.baidu.mapapi.map.MapView;
+import com.baidu.mapapi.map.MyLocationData;
+import com.baidu.mapapi.model.LatLng;
 import com.example.administrator.baidutext.utils.ActivityUtils;
-
 
 import java.util.ArrayList;
 import java.util.List;
@@ -29,19 +34,32 @@ import butterknife.ButterKnife;
 public class HomeActivity extends AppCompatActivity {
     @BindView(R.id.position_text_view)
     TextView positionText;
+    @BindView(R.id.bmapView)
+    MapView mapView;
     private ActivityUtils mActivityUtils;
     public LocationClient mLocationClient;
+    private BaiduMap baiduMap;
+    private boolean isFirstLocate=true;
 
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        SDKInitializer.initialize(MyApplication.getContext());
         setContentView(R.layout.activity_home);
         ButterKnife.bind(this);
         mActivityUtils = new ActivityUtils(this);
         mLocationClient = new LocationClient(getApplicationContext());
-        mLocationClient.registerLocationListener(new MyLocationClient());
+        mLocationClient.registerLocationListener(listener);
+        baiduMap=mapView.getMap();
+        baiduMap.setMyLocationEnabled(true);
+        //权限获取的方法
+        permissionGain();
 
+
+    }
+
+    private void permissionGain() {
         List<String> permissionList = new ArrayList<>();
         if (ContextCompat.checkSelfPermission(HomeActivity.this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
             permissionList.add(Manifest.permission.ACCESS_FINE_LOCATION);
@@ -57,27 +75,36 @@ public class HomeActivity extends AppCompatActivity {
         }
         if (!permissionList.isEmpty()) {
             String[] pewmissions = permissionList.toArray(new String[permissionList.size()]);
-            ActivityCompat.requestPermissions(HomeActivity.this, pewmissions, 1);
+            ActivityCompat.requestPermissions(HomeActivity.this, pewmissions, 1);//一次性申请所有没有申请的权限
 
         } else {
 
             requestLocation();
         }
-
-
     }
 
     private void requestLocation() {
+        initLocation();
         mLocationClient.start();
+    }
+
+    private void initLocation() {
+        LocationClientOption option = new LocationClientOption();
+        option.setScanSpan(5000);//每隔5秒更新一次位置信息
+        option.setLocationMode(LocationClientOption.LocationMode.Device_Sensors);
+        option.setIsNeedAddress(true);
+        option.setCoorType("bd09ll");
+        option.setOpenGps(true);
+        mLocationClient.setLocOption(option);
     }
 
     @Override
     public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
         switch (requestCode) {
             case 1:
-                if(grantResults.length>0){
-                    for (int result: grantResults){
-                        if(result!=PackageManager.PERMISSION_GRANTED){
+                if (grantResults.length > 0) {
+                    for (int result : grantResults) {
+                        if (result != PackageManager.PERMISSION_GRANTED) {
                             Toast.makeText(this, "必须统一此权限才能使用", Toast.LENGTH_SHORT).show();
                             finish();
                             return;
@@ -85,33 +112,61 @@ public class HomeActivity extends AppCompatActivity {
                     }
                     requestLocation();
 
-                }else {
+                } else {
                     Toast.makeText(this, "未知的错误发生了", Toast.LENGTH_SHORT).show();
                     finish();
                 }
-            break;
+                break;
             default:
                 break;
         }
     }
 
-    private class MyLocationClient implements BDLocationListener {
-        @Override
-        public void onReceiveLocation(BDLocation location) {
-            StringBuilder currentPosition = new StringBuilder();
-            currentPosition.append("纬度：").append(location.getLatitude()).append("\n");
-            currentPosition.append("经线：").append(location.getLongitude()).append("\n");
-            currentPosition.append("定位方式:");
-            //设置定位方式
-            if (location.getLocType() == BDLocation.TypeGpsLocation) {
-                currentPosition.append("GPS");
-            } else if (location.getLocType() == BDLocation.TypeNetWorkLocation) {
-                currentPosition.append("网络");
-            }
-            //将定位信息显示到屏幕上
-            positionText.setText(currentPosition);
+   private BDLocationListener listener=new BDLocationListener() {
+       @Override
+       public void onReceiveLocation(BDLocation location) {
+           if(location.getLocType()==BDLocation.TypeGpsLocation || location.getLocType()==BDLocation.TypeNetWorkLocation){
+               navigateTo(location);
+           }
+       }
+   };
+
+    private void navigateTo(BDLocation location) {
+        if(isFirstLocate){
+            LatLng ll=new LatLng(location.getLatitude(),location.getLongitude());
+            MapStatusUpdate update= MapStatusUpdateFactory.newLatLng(ll);
+            baiduMap.animateMapStatus(update);
+            update=MapStatusUpdateFactory.zoomTo(16f);
+            baiduMap.animateMapStatus(update);
+            isFirstLocate=false;
+
         }
+        MyLocationData locationBuilder =new MyLocationData.Builder()
+                .latitude(location.getLatitude())
+                .longitude(location.getLongitude())
+                .accuracy(100f)
+                .build();
+        baiduMap.setMyLocationData(locationBuilder);
+
     }
 
+    @Override
+    protected void onResume() {
+        super.onResume();
+        mapView.onResume();
+    }
 
+    @Override
+    protected void onPause() {
+        super.onPause();
+        mapView.onPause();
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        mLocationClient.stop();
+        mapView.onDestroy();
+        baiduMap.setMyLocationEnabled(false);
+    }
 }
--- app/src/main/java/com/example/administrator/baidutext/MyApplication.java
+++ app/src/main/java/com/example/administrator/baidutext/MyApplication.java
@@ -3,6 +3,8 @@ package com.example.administrator.baidutext;
 import android.app.Application;
 import android.content.Context;
 
+import com.baidu.mapapi.SDKInitializer;
+
 /**
  * Created by Administrator on 2017/1/16.
  */
@@ -13,6 +15,7 @@ public class MyApplication extends Application {
     public void onCreate() {
         super.onCreate();
         context=getApplicationContext();
+
     }
     public static  Context getContext(){
         return context;
